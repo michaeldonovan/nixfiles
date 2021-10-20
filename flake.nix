c3a97b40be003e70f2abdc3ceb45ac37dc6b6067
Michael Donovan <michael@mdonovan.dev>
2021-10-20 12:57:47 -0500

    flake fixes

--- flake.nix
+++ flake.nix
@@ -1,43 +1,50 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus"; 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
 
-    home-manager = { 
-      url = "github:nix-community/home-manager"; 
-      inputs.nixpkgs.follows = "nixpkgs"; 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur = { 
-      url = "github:nix-community/NUR"; 
-      inputs.nixpkgs.follows = "nixpkgs"; 
-    };  
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, utils, nur, home-manager, ... }:
     utils.lib.mkFlake {
       inherit self inputs;
 
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
 
       sharedOverlays = [
-        self.overlay
         inputs.nur.overlay
       ];
 
       channelsConfig.allowUnfree = true;
 
-      hosts = {
-        monolith.modules = [
-          ./hosts/monolith/configuration.nix
-          ./hosts/monolith/home/home.nix
-          home-manager.nixosModule
-        ];
+      hostDefaults.system = "x86_64-linux";
 
-        MacBook-Air.modules = [
-          ./hosts/MacBook-Air/home/home.nix
-          home-manager.darwinModule
-        ];
+      hosts = {
+        monolith = {
+          modules = [
+            ./hosts/monolith/configuration.nix
+            ./hosts/monolith/home/home.nix
+            home-manager.nixosModule
+          ];
+        };
+
+        MacBook-Air = {
+          system = "aarch64-darwin";
+          output = "darwinConfigurations";
+          #builder = args: nix-darwin.lib.darwinSystem (removeAttrs args [ "system" ]);
+          modules = [
+            ./hosts/MacBook-Air/home/home.nix
+            home-manager.darwinModule
+          ];
+        };
       };
 
       outputsBuilder = channels: with channels.nixpkgs;{
