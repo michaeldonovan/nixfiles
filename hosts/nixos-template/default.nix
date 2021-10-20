6ec7ee2e6b658b606fd7b04e40b34e5bcb906f7e
Michael Donovan <michael@mdonovan.dev>
2021-10-20 18:12:52 -0500

    disable power management on vm template

--- hosts/nixos-template/default.nix
+++ hosts/nixos-template/default.nix
@@ -67,20 +67,7 @@ in
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  powerManagement.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
