ea183ab2c3d6304cb02f75cf6ea4832e736dae8c
Michael Donovan <michael@mdonovan.dev>
2021-11-07 18:21:41 -0600

    setup root ssh keys

--- modules/users.nix
+++ modules/users.nix
@@ -1,6 +1,15 @@
 { config, pkgs, lib, ... }:
+let
+  ssh-keys = [
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHqLljawKuGhWuLpiFTs3cFj6sezvD0rVymaQ631QFZ4jHiJ5pqqyegWUcjGstxMRRymgPu2+evM3sKCvDo6/eU= "
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUcN5An0gqAEH56hNzIMw8RmDGqSY/uGoenPskUHPM "
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0ORCwUG71gRB3+qMx7ASej0px/aCdSnYUSs8EAIpne "
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJIIfg16lNol+oHsSCwunDLSK4EY0IFXXBnoCXFUY2Ov "
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIG822TCsI9xe1AxoslofXMeBQnySxrn0xV9yHVAewtDVAAAABHNzaDo=  "
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILsIPaApMqFdnDO+w0dZ2GgY4tWjFQylCgV8Blg16p0V "
+  ];
+in
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.michael = {
     isNormalUser = true;
     home = "/home/michael";
@@ -9,14 +18,12 @@
     extraGroups = [
       "wheel"
     ];
-    openssh.authorizedKeys.keys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHqLljawKuGhWuLpiFTs3cFj6sezvD0rVymaQ631QFZ4jHiJ5pqqyegWUcjGstxMRRymgPu2+evM3sKCvDo6/eU= "
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUcN5An0gqAEH56hNzIMw8RmDGqSY/uGoenPskUHPM "
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC0ORCwUG71gRB3+qMx7ASej0px/aCdSnYUSs8EAIpne "
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJIIfg16lNol+oHsSCwunDLSK4EY0IFXXBnoCXFUY2Ov "
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIG822TCsI9xe1AxoslofXMeBQnySxrn0xV9yHVAewtDVAAAABHNzaDo=  "
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILsIPaApMqFdnDO+w0dZ2GgY4tWjFQylCgV8Blg16p0V "
-    ];
+    openssh.authorizedKeys.keys = ssh-keys;
+  };
+
+  users.users.root = {
+    shell = pkgs.fish;
+    openssh.authorizedKeys.keys = ssh-keys;
   };
 
   environment.homeBinInPath = true;
