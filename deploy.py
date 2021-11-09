231834dd6c7da88bf1a6a57464725aa9c964c45c
Michael Donovan <michael@mdonovan.dev>
2021-11-09 12:35:34 -0600

    deploy.py cleanup

--- deploy.py
+++ deploy.py
@@ -6,12 +6,70 @@ import platform
 import argparse
 import subprocess
 import json
+from multiprocessing import cpu_count
+from typing import List
 from termcolor import colored
 from tabulate import tabulate
 from paramiko import SSHConfig
 
 
-def get_hostname(ssh_config, host):
+def format_result(result):
+    if result:
+        return colored('OK', 'green', attrs=['bold'])
+    else:
+        return colored('ERR', 'red', attrs=['bold'])
+
+
+def list_hosts_in_flake(flake: str) -> List[str]:
+    """Get a list of hosts with nixosConfigurations provided by a given flake
+
+    Args:
+        flake (str): Path to a nix flake
+
+    Returns:
+        List[str]: List of hosts in flake
+    """
+    flake_output = subprocess.check_output(
+        ['nix', 'flake', 'show', flake, '--json'], stderr=subprocess.DEVNULL)
+    flake_json = json.loads(flake_output)
+    hosts = [host for host in flake_json['nixosConfigurations']]
+    return hosts
+
+
+def ping(hostname: str) -> bool:
+    """Attempts to ping a given host
+
+    Args:
+        hostname (str): Hostname of the host to ping
+
+    Returns:
+        bool: True if host was able to be pinged, False otherwise
+    """
+    return os.system(f"ping -c 1 {hostname} >/dev/null 2>&1") == 0
+
+
+def get_ssh_config() -> SSHConfig:
+    """Builds an SSHConfig object based on the contents of ~/.ssh/config
+
+    Returns:
+        SSHConfig: 
+    """
+    ssh_config = SSHConfig()
+    ssh_config_path = os.path.join(os.path.expanduser('~'), '.ssh', 'config')
+    ssh_config.parse(open(ssh_config_path))
+    return ssh_config
+
+
+def get_hostname(ssh_config: SSHConfig, host: str) -> str:
+    """Attempts to get the hostname of a given host from an ssh config
+
+    Args:
+        ssh_config (SSHConfig): The ssh config object
+        host (str): The name of the host as listed in the ssh config
+
+    Returns:
+        str: The hostname or None
+    """
     if host == platform.node():
         return 'localhost'
 
@@ -22,32 +80,37 @@ def get_hostname(ssh_config, host):
         return None
 
 
-def format_result(result):
-    if result:
-        return colored('OK', 'green')
-    else:
-        return colored('ERR', 'red')
+def deploy_host(flake: str, host: str, hostname: str) -> bool:
+    """Builds and deploys the configuration for a given host
+
+    Args:
+        flake (str): Path to a nix flake to build
+        host (str): Name of the host as listed in the flake
+        hostname (str): Hostname or IP address of the host
+
+    Returns:
+        bool: 
+    """
+    command = f'sudo nixos-rebuild switch \
+                -j {cpu_count()} \
+                --flake {flake}#{host} \
+                --target-host {hostname} \
+                --build-host localhost'
+    return os.system(command) == 0
 
 
-def list_hosts_in_flake(flake):
-    flake_json = json.loads(subprocess.check_output(
-        ['nix', 'flake', 'show', flake, '--json']))
-    hosts = [host for host in flake_json['nixosConfigurations']]
-    return hosts
+def deploy(hosts: str, flake: str):
+    """Runs nixos-rebuild on a given and deploys configuration to hosts
 
-
-def ping(hostname):
-    return os.system(f"ping -c 1 {hostname} >/dev/null 2>&1") == 0
-
-
-def deploy(hosts, flake):
+    Args:
+        hosts (str): List of hosts to be deployed. Hosts must be listed in flake
+        flake (str): Path to the nix flake to build
+    """
     flake = os.path.abspath(flake)
     if not hosts:
         hosts = list_hosts_in_flake(flake)
 
-    ssh_config = SSHConfig()
-    ssh_config_path = os.path.join(os.path.expanduser('~'), '.ssh', 'config')
-    ssh_config.parse(open(ssh_config_path))
+    ssh_config = get_ssh_config()
 
     results = {}
     for host in hosts:
@@ -56,15 +119,14 @@ def deploy(hosts, flake):
             colored(
                 f'------ Building for {host} ({hostname}) ------\n',
                 attrs=['reverse']))
+
         if ping(hostname):
-            results[host] = os.system(
-                f'sudo nixos-rebuild switch -j $(nproc) --flake {flake}#{host} --target-host {hostname} --build-host localhost') == 0
+            results[host] = deploy_host(flake, host, hostname)
         else:
-            print(colored('error:','red') +
-                  ' host at \'' +
-                  colored(f'{hostname}', 'magenta') +
-                  '\' is unreachable ')
+            print(colored('error:', 'red', attrs=['bold']) + ' host at \'' + colored(
+                f'{hostname}', 'magenta', attrs=['bold']) + '\' is unreachable ')
             results[host] = False
+
         print()
 
     print(tabulate([[host, format_result(result)]
@@ -72,9 +134,12 @@ def deploy(hosts, flake):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('hosts', nargs='*', help='hosts to deploy')
-    parser.add_argument("--flake", default='.', help='path to flake')
+    parser = argparse.ArgumentParser(
+        description='Builds and deploys NixOS configuration in a Nix flake')
+    parser.add_argument(
+        'hosts', nargs='*', help='hosts to deploy. If none are provided all hosts in flake will be deployed')
+    parser.add_argument("--flake", default='.',
+                        help='path to flake. Will default to current directory if not provided')
     args = parser.parse_args()
     try:
         deploy(hosts=args.hosts, flake=args.flake)
