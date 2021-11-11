d0e7cc704346a976b3bdff67915185da8bc07408
Michael Donovan <michael@mdonovan.dev>
2021-11-10 19:37:43 -0600

    enable xdg portal

--- hosts/monolith/default.nix
+++ hosts/monolith/default.nix
@@ -98,6 +98,8 @@ in
 
   programs.dconf.enable = true;
 
+  xdg.portal.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
