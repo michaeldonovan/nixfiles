cedd930eaef7a71c027b93089bb0909111e7d1b5
Michael Donovan <michael@mdonovan.dev>
2021-12-27 17:04:26 -0600

    add wg interface to algiers

--- hosts/algiers/wireguard.nix
+++ hosts/algiers/wireguard.nix
@@ -21,7 +21,29 @@ in
       peers = [
         {
           publicKey = "jYhfXlZs/VdV0XQxQrxdS8JbdEso/ACTfhShpEDkqz4=";
-          allowedIPs = [ "10.100.0.1/32" ];
+          allowedIPs = [ "10.100.0.2/32" ];
+        }
+      ];
+    };
+    wg1 = {
+      ips = [ "10.101.0.1/24" ];
+
+      listenPort = 51821;
+
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.101.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.101.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      privateKeyFile = "/secrets/wireguard-keys/private";
+
+      peers = [
+        {
+          publicKey = "fNyNlQSDREYWPJn1d61SeQU/rQx+jdapFpdM02aQ130=";
+          allowedIPs = [ "10.101.0.2/32" ];
         }
       ];
     };
