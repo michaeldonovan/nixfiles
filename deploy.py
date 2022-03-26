d0e4157961816047a84838b343869f10d3fcfd9e
Michael Donovan <michael@mdonovan.dev>
2022-03-26 12:10:00 -0500

    Add build command printout to deploy.py

--- deploy.py
+++ deploy.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
+import json
 import os
 import platform
-import argparse
 import subprocess
-import json
 import sys
 from multiprocessing import cpu_count
-from typing import List
-from termcolor import colored
-from tabulate import tabulate
-from paramiko import SSHConfig
+from typing import List, Optional
 
-LOCALHOST = 'localhost'
+from paramiko import SSHConfig
+from tabulate import tabulate
+from termcolor import colored
+
+LOCALHOST = "localhost"
 ERR_CODE = 1
 OK_CODE = 0
 
 
 def format_result(result):
     if result:
-        return colored('OK', 'green', attrs=['bold'])
+        return colored("OK", "green", attrs=["bold"])
     else:
-        return colored('ERR', 'red', attrs=['bold'])
+        return colored("ERR", "red", attrs=["bold"])
 
 
 def list_hosts_in_flake(flake: str) -> List[str]:
@@ -35,9 +36,10 @@ def list_hosts_in_flake(flake: str) -> List[str]:
         List[str]: List of hosts in flake
     """
     flake_output = subprocess.check_output(
-        ['nix', 'flake', 'show', flake, '--json'], stderr=subprocess.DEVNULL)
+        ["nix", "flake", "show", flake, "--json"], stderr=subprocess.DEVNULL
+    )
     flake_json = json.loads(flake_output)
-    hosts = [host for host in flake_json['nixosConfigurations']]
+    hosts = [host for host in flake_json["nixosConfigurations"]]
     return hosts
 
 
@@ -57,15 +59,15 @@ def get_ssh_config() -> SSHConfig:
     """Builds an SSHConfig object based on the contents of ~/.ssh/config
 
     Returns:
-        SSHConfig: 
+        SSHConfig:
     """
     ssh_config = SSHConfig()
-    ssh_config_path = os.path.join(os.path.expanduser('~'), '.ssh', 'config')
+    ssh_config_path = os.path.join(os.path.expanduser("~"), ".ssh", "config")
     ssh_config.parse(open(ssh_config_path))
     return ssh_config
 
 
-def get_hostname(ssh_config: SSHConfig, host: str) -> str:
+def get_hostname(ssh_config: SSHConfig, host: str) -> Optional[str]:
     """Attempts to get the hostname of a given host from an ssh config
 
     Args:
@@ -80,7 +82,7 @@ def get_hostname(ssh_config: SSHConfig, host: str) -> str:
 
     host_config = ssh_config.lookup(host)
     if host_config:
-        return host_config['hostname']
+        return host_config["hostname"]
     else:
         return None
 
@@ -94,13 +96,10 @@ def deploy_host(flake: str, host: str, hostname: str) -> bool:
         hostname (str): Hostname or IP address of the host
 
     Returns:
-        bool: 
+        bool:
     """
-    command = f'sudo nixos-rebuild switch \
-                -j {cpu_count()} \
-                --flake {flake}#{host} \
-                --target-host {hostname} \
-                --build-host localhost'
+    command = f"sudo nixos-rebuild switch -j {cpu_count()} --flake {flake}#{host} --target-host {hostname} --build-host localhost"
+    print("Build command: " + colored(command, "blue") + "\n")
     return os.system(command) == OK_CODE
 
 
@@ -122,48 +121,60 @@ def deploy(hosts: List[str], flake: str):
         hostname = get_hostname(ssh_config, host)
         print(
             colored(
-                f'\n------ Building for {host} ({hostname}) ------\n',
-                attrs=['reverse']))
+                f"\n------ Building for {host} ({hostname}) ------\n", attrs=["reverse"]
+            )
+        )
 
-        if ping(hostname):
+        if hostname and ping(hostname):
             results[host] = deploy_host(flake, host, hostname)
         else:
-            print(colored('error:', 'red', attrs=['bold']) + ' host at \'' + colored(
-                f'{hostname}', 'magenta', attrs=['bold']) + '\' is unreachable ')
+            print(
+                colored("error:", "red", attrs=["bold"])
+                + " host at '"
+                + colored(f"{hostname}", "magenta", attrs=["bold"])
+                + "' is unreachable "
+            )
             results[host] = False
 
         print()
 
-    print(tabulate([[host, format_result(result)]
-          for host, result in results.items()]))
+    print(tabulate([[host, format_result(result)] for host, result in results.items()]))
 
 
 def deploy_darwin(hosts: List[str], flake: str):
-    """Runs darwin-rebuild 
+    """Runs darwin-rebuild
 
     Args:
         hosts (List[str]): List of hosts to be deployed. Will only succeed if only host is localhost
         flake (str): Path to the nix flake to build
     """
-    hostname = get_hostname(hosts[0]) if hosts else LOCALHOST
+    hostname = get_hostname(get_ssh_config(), hosts[0]) if hosts else LOCALHOST
     if len(hosts) > 1 or hostname != LOCALHOST:
-        print(colored('error:', 'red', attrs=[
-              'bold']) + ' remote deployment not supported on macOS')
+        print(
+            colored("error:", "red", attrs=["bold"])
+            + " remote deployment not supported on macOS"
+        )
         return ERR_CODE
 
-    command = f'darwin-rebuild switch \
-                -j {cpu_count()} \
-                --flake {flake}'
+    command = f"darwin-rebuild switch -j {cpu_count()} --flake {flake}"
+    print("Build command: " + colored(command, "blue") + "\n")
     return os.system(command)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Builds and deploys NixOS configuration in a Nix flake')
+        description="Builds and deploys NixOS configuration in a Nix flake"
+    )
     parser.add_argument(
-        'hosts', nargs='*', help='hosts to deploy. If none are provided all hosts in flake will be deployed')
-    parser.add_argument("--flake", default='.',
-                        help='path to flake. Will default to current directory if not provided')
+        "hosts",
+        nargs="*",
+        help="hosts to deploy. If none are provided all hosts in flake will be deployed",
+    )
+    parser.add_argument(
+        "--flake",
+        default=".",
+        help="path to flake. Will default to current directory if not provided",
+    )
     args = parser.parse_args()
 
     exit_code = ERR_CODE
