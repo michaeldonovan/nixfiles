e765075e5672286b9ba144b1f8723cf2d820a494
Michael Donovan <michael@mdonovan.dev>
2022-06-25 20:58:04 -0500

    switch to track nixpkgs 22.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     musnix.url = "github:musnix/musnix";
 
