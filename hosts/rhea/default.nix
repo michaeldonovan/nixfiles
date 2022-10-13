b574da35639370baa0d22d02aa3895e99b7cc99b
Michael Donovan <michael@mdonovan.dev>
2022-10-12 20:06:12 -0500

    rhea nfs mounts

--- hosts/rhea/default.nix
+++ hosts/rhea/default.nix
@@ -1,13 +1,9 @@
 { config, ... }:
-let
-  # this line prevents hanging on network split
-  credentialsFile = "/secrets/smb-secrets";
-  opts = "uid=nobody,gid=users,noperm,credentials=${credentialsFile},x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-in
 {
   imports =
     [
     ];
+
     boot.supportedFilesystems = [ "zfs" ];
     boot.kernelParams = [ "zfs.zfs_arc_max=512000000" ];
     networking.hostId = "5595a05c";
@@ -18,26 +14,16 @@ in
 
   services.rpcbind.enable = true; # needed for NFS
 
-  /*
   fileSystems."/rendon/Plex" = {
     device = "192.168.1.146:/mnt/rendon/Plex";
-    fsType = "smb";
     fsType = "nfs";
   };
   fileSystems."/rendon/Downloads" = {
     device = "192.168.1.146:/mnt/rendon/Downloads";
     fsType = "nfs";
   };
-  */
-
-  fileSystems."/rendon/Downloads" = {
-    device = "//192.168.1.156/Downloads";
-    fsType = "cifs";
-    options = [ "${opts}" ];
-  };
-  fileSystems."/rendon/Plex" = {
-    device = "//192.168.1.156/Plex";
-    fsType = "cifs";
-    options = [ "${opts}" ];
+  fileSystems."/rendon/Books" = {
+    device = "192.168.1.146:/mnt/rendon/Books";
+    fsType = "nfs";
   };
 }
