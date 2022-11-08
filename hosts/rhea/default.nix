0d26b2d9dbee1055146ca897e18fd93aa9dcc743
Michael Donovan <michael@mdonovan.dev>
2022-11-07 20:49:53 -0600

    Update rhea NFS mount options, use summit-user.nix

--- hosts/rhea/default.nix
+++ hosts/rhea/default.nix
@@ -1,7 +1,10 @@
 { config, ... }:
 
 let
-  nfsOpts = "x-systemd.automount";
+  nfsOpts = "x-systemd.automount,x-systemd.idle-timeout=60,x-systemd.device-timeout=175,x-systemd.mount-timeout=5s,noauto,soft";
+  smbCredentialsFile = "/secrets/smb-secrets";
+  smbOpts = "uid=1000,gid=100,credentials=${smbCredentialsFile},${nfsOpts}";
+
 in
 {
   imports =
@@ -18,66 +21,67 @@ in
   services.sanoid = {
     enable = true;
     datasets = {
+      "rhea" = {
+        autoprune = true;
+        daily = 7;
+        recursive = true;
+      };
       "rhea/appdata" = {
         autoprune = true;
         daily = 3;
-        hourly = 6;
+        hourly = 24;
         recursive = true;
       };
-      "rhea/email" = {
-        autoprune = true;
-        daily = 7;
-      };
     };
-  };
-
-  users.users.summit = {
-    isNormalUser = true;
-    extraGroups = [
-    ];
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7wfXoUI/CLGC1v/6SN49b5wm+NzcKA9SFb/azJU6wELVeP99bZ+VCQZ2koUI9UJAkJ84a/R80G2fYJ9qYnLsiNi+n8Yib4FBQ+gGcsG6DdK4SidjH3aBFdxKOKFq7mHGyq6Tl0csB6prAiUcDEgsBkpmFjfD66LpZPXquQW90W3C0YTNZ/LdhYUYbOg+RS6cPlY7nPttNhoZCWDgw5G8K8/5HlHJtnSdCosJNcWfPYNrNHqMqq1ivaSIVBXU0cgzULZ0uWxT+Y5WHDrHRXa5WmzXo2xDPiT+fnaun2x0qeexah6KjFwbks1829WReTNtkqfeoQ/Fi3AFTl1kUlwh3Op1kuvEKvRoU9pH9GwNZhJGV3My5/yPRCNn3N9Fs7oXRLNfzhuS0dqpl3FTG9/sGV5gavCRUG0hezJNDuf+kGraUeIEDCysVLriSX0cWk3oPmj/Q0YMsbSQpHfIENKDxm8Mp8NNz39FlfFWxwVZxlpCKBpjigCdsFbgGs+X19/k=" ];
   };
 
   services.rpcbind.enable = true; # needed for NFS
 
   fileSystems."/rendon/Plex" = {
     device = "192.168.1.146:/mnt/rendon/Plex";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
   fileSystems."/rendon/Downloads" = {
     device = "192.168.1.146:/mnt/rendon/Downloads";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
   fileSystems."/rendon/Books" = {
     device = "192.168.1.146:/mnt/rendon/Books";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
   fileSystems."/rendon/Backups" = {
     device = "192.168.1.146:/mnt/rendon/Backups";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
   fileSystems."/rendon/borg" = {
     device = "192.168.1.146:/mnt/rendon/borg";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
+  /*
+    fileSystems."/rendon/borg" = {
+    device = "//192.168.1.146/borg";
+    fsType = "cifs";
+    options = [ "${smbOpts}" ];
+    };
+  */
   fileSystems."/rendon/Pictures" = {
     device = "192.168.1.146:/mnt/rendon/Pictures";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" ];
   };
   fileSystems."/rendon/Music" = {
     device = "192.168.1.146:/mnt/rendon/Music";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" "ro" ];
   };
   fileSystems."/rendon/Documents" = {
     device = "192.168.1.146:/mnt/rendon/Documents";
-    fsType = "nfs";
+    fsType = "nfs4";
     options = [ "${nfsOpts}" "ro" ];
   };
 
