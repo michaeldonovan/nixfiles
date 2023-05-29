24c0306e6b89092a435e20e3a3942cf5259dddd5
Michael Donovan <michael@mdonovan.dev>
2023-05-29 12:06:30 -0500

    Fix fish syntax errror

--- home/fish.nix
+++ home/fish.nix
@@ -13,7 +13,7 @@
       if [ $TERM = "xterm-kitty" ]
           alias ssh="kitty +kitten ssh"
       end
-      if [ test -e $HOME/miniconda3/bin/conda ]
+      if test -e $HOME/miniconda3/bin/conda 
         eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
       end
     '';
