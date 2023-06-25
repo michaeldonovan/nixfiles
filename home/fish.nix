296f59524c341cf4166fe3439ac9df18d38dab46
Michael Donovan <michael@mdonovan.dev>
2023-06-25 11:36:32 -0500

    update fish functions/plugins

--- home/fish.nix
+++ home/fish.nix
@@ -47,71 +47,57 @@
       git = "hub";
     };
 
+    functions = {
+      docker-update.body = ''
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+                           containrrr/watchtower \
+                                              --run-once
+                                              docker image prune -af
+      '';
+      fish_title.body = ''
+        if [ $_ != "fish" ]
+            echo $_
+        else 
+            prompt_pwd
+        end
+      '';
+      nix-rebuild.body = ''
+        if [ (uname) = "Darwin" ]
+            pushd ~/nixfiles 
+            darwin-rebuild --flake . -j (nproc) $argv
+            pushd
+        else 
+            sudo nixos-rebuild switch --flake ~/nixfiles -j (nproc) 
+        end
+      '';
+
+    };
+
     plugins = [
-      # {
-      #     name = "fasd";
-      #     src = pkgs.fetchFromGitHub {
-      #         owner = "oh-my-fish";
-      #         repo = "plugin-fasd";
-      #         rev = "98c4c729780d8bd0a86031db7d51a97d55025cf5";
-      #         sha256 = "0m0q0x66b498lxmma9l9qxpzfkms4g7mg26xb6kh2p55vil1547h";
-      #     };
-      # }
-      # {
-      #     name = "vi-mode";
-      #     src = pkgs.fetchFromGitHub {
-      #         owner = "oh-my-fish";
-      #         repo = "plugin-vi-mode";
-      #         rev = "2655a7253077faefd2f0a57b799dee687344d4fb";
-      #         sha256 = "1ww4z8ahx8ag38vbfccrfd9chv1gkg35fvwnnxwybyak6mnic5li";
-      #     };
-      # }
-      # {
-      #     name = "simple-ass-prompt";
-      #     src = pkgs.fetchFromGitHub {
-      #         owner = "lfiolhais";
-      #         repo = "theme-simple-ass-prompt";
-      #         rev = "b2d3ed234a0f9528369f5dc6452e2b1c6ca1b23e";
-      #         sha256 = "1shasf1bx5zw99qccqpxpvl29h1jczhx33hi6fahjwp01x1d81cl";
-      #     };
-      # }
-      # {
-      #     name = "foreign-env";
-      #     src = pkgs.fetchFromGitHub {
-      #         owner = "oh-my-fish";
-      #         repo = "plugin-foreign-env";
-      #         rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
-      #         sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
-      #     };
-      # }
+      {
+        name = "bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "f3a547b0239cf8529d35c1922dd242bacf751d3b";
+          sha256 = "3mFlFiqGfQ+GfNshwKfhQ39AuNMdt8Nv2Vgb7bBV7L4=";
+        };
+      }
+      {
+        name = "github-copilot-cli";
+        src = pkgs.fetchFromGitHub {
+          owner = "z11i";
+          repo = "github-copilot-cli.fish";
+          rev = "ccb6367bbb3055ea19b9ff0eac1ccf1c5e86419a";
+          sha256 = "cnmxvnG3WN2uqtt1aUEf0OFJulQSpFd3RJeeVKpDI9Y=";
+        };
+      }
     ];
   };
 
   xdg.configFile."fish/conf.d/fish_user_key_bindings.fish".text = ''
     function fish_user_key_bindings
         fish_vi_key_bindings
-    end
-  '';
-
-  xdg.configFile."fish/functions/fish_title.fish".text = ''
-    function fish_title 
-        if [ $_ != "fish" ]
-            echo $_
-        else 
-            prompt_pwd
-        end
-    end
-  '';
-
-  xdg.configFile."fish/functions/nix-rebuild.fish".text = ''
-    function nix-rebuild
-        if [ (uname) = "Darwin" ]
-            pushd ~/nixfiles 
-            darwin-rebuild --flake . -j (nproc) $argv
-            pushd
-        else 
-            sudo nixos-rebuild -j (nproc) $argv
-        end
     end
   '';
 }
