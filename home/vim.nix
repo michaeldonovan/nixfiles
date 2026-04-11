f91ff235d7d69b3c9f5976acd403bb29231173f1
Michael Donovan <michael@mdonovan.dev>
2026-04-11 15:40:13 -0500

    nix lsp

--- home/vim.nix
+++ home/vim.nix
@@ -1,6 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.nvchad.enable = true;
+  programs.nvchad = {
+    enable = true;
+    extraPackages = with pkgs; [
+      nixd
+      nixfmt-rfc-style
+    ];
+    extraConfig = ''
+      local nixd_config = {
+        cmd = { "nixd" },
+        settings = {
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            formatting = {
+              command = { "nixfmt" },
+            },
+          },
+        },
+      }
+
+      if vim.lsp and vim.lsp.config then
+        vim.lsp.config("nixd", nixd_config)
+        vim.lsp.enable("nixd")
+      else
+        local ok, lspconfig = pcall(require, "lspconfig")
+        if ok then
+          lspconfig.nixd.setup(nixd_config)
+        end
+      end
+    '';
+  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
